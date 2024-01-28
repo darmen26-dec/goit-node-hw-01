a27a4887fadabf1b128188b58439138fc7226c09
darmen26-dec <dariamendelewska@gmail.com>
2024-01-28 16:02:48 +0100

    hw done

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1>Task results:</h1>
+
+# Otrzymujemy i wyprowadzamy całą listę kontaktów w postaci tabeli (console.table)
+
+node index.js --action list
+
+![Lista kontaktów](https://ibb.co/FzksBrs)
+
+# Otrzymujemy kontakt po id
+
+node index.js --action get --id 05olLMgyVQdWRwgKfg5J6
+
+![Pobierz kontakt](https://ibb.co/0rgbRHn)
+
+# Dodajemy kontakt
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+![Dodaj kontakt](https://ibb.co/9tbygB7)
+
+# Usuwamy kontakt
+
+node index.js --action remove --id qdggE76Jtbfd9eWJHrssH
+
+![Usuń kontakt](https://ibb.co/4Z1R7Lf)
---
+++ contacts.js
@@ -0,0 +1,78 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const contactsPath = path.join(__dirname, './db/contacts.json');
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(data);
+    return contacts || [];
+  } catch (error) {
+    console.log('Reading contact list error:', error.message);
+    return [];
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact || null;
+  } catch (error) {
+    console.log('Error reading file contacts', error.message);
+    return null;
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const contactToDelete = contacts.find(
+      (contact) => contact.id === contactId
+    );
+    const contactsUpdate = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+
+    await fs.writeFile(contactsPath, JSON.stringify(contactsUpdate, null, 2));
+    if (!contactToDelete) {
+      return null;
+    } else {
+      console.log(`Contact with id ${contactId} has been removed`);
+      return contactsUpdate;
+    }
+  } catch (error) {
+    console.error('Error reading file contacts', error.message);
+    return null;
+  }
+};
+
+const addContact = async (name, email, phone) => {
+  try {
+    if (!name || !email || !phone) {
+      console.log('Name, email, and phone are required.');
+      return null;
+    }
+
+    const newContact = { id: uuidv4(), name, email, phone };
+    const contacts = await listContacts();
+    const updatedContacts = [...contacts, newContact];
+
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+    console.log(`Contact "${name}" with id ${newContact.id} has been added.`);
+
+    return newContact;
+  } catch (error) {
+    console.log('Error reading file contacts', error.message);
+    return null;
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
